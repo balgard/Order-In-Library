c528ad273c38be78895600635956e5157b030d0a
balgard <student@balgard.local>
2018-03-15 11:15:00 -0500

    book object test

---
+++ Book.swift
@@ -0,0 +1,24 @@
+//
+//  Book.swift
+//  Order In Library
+//
+//  Created by Balgard on 3/14/18.
+//  Copyright © 2018 Balgard. All rights reserved.
+//
+
+import UIKit
+
+class Book: UIView
+{
+    var color = ""
+    var id = ""
+    var child = UIView()
+    
+    func setup(color: String, id: String, child: UIView)
+    {
+        self.color = color
+        self.id = id
+        self.child = child
+    }
+
+}
